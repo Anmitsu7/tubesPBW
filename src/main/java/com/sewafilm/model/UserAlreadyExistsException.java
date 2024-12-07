ac1d3b0ef2135addf6814ac7ddbdc0da9fbff4a4
KenzhuMatthew <6182101063@student.unpar.ac.id>
2024-12-07 18:09:46 +0700

    Delete src/main directory

--- src/main/java/com/sewafilm/model/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.sewafilm.model;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
