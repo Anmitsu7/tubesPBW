98aba882060e6e65d917c6f0a2544a712388abe7
unknown <6182101063@student.unpar.ac.id>
2025-01-12 22:38:38 +0700

    Update Terbaru

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -72,6 +72,7 @@ public class SecurityConfig {
                            "/webjars/**",
                            "/fonts/**",
                            "/covers/**",
+                           "/film/**",
                            "/actors/**"
                        ).permitAll()
                        // Admin URLs
